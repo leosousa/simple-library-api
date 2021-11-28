612edd20c6e6cc881507b79591b92e0652a42fcd
Leonardo Sousa <leonardofsousa@outlook.com>
2021-11-28 00:13:09 -0300

    adicionando pacote NetDevPack e ajustando classes do shared do dominio

--- SimpleLibrary.Domain/Shared/Events/StoredEvent.cs
+++ SimpleLibrary.Domain/Shared/Events/StoredEvent.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NetDevPack.Messaging;
 
-namespace SimpleLibrary.Domain.Shared.Events
+namespace SimpleLibrary.Domain.Shared.Events;
+
+public class StoredEvent : Event
 {
-    internal class StoredEvent
+    public StoredEvent(Event theEvent, string data, string user)
     {
+        Id = Guid.NewGuid();
+        AggregateId = theEvent.AggregateId;
+        MessageType = theEvent.MessageType;
+        Data = data;
+        User = user;
     }
+
+    // EF Constructor
+    protected StoredEvent() { }
+
+    public Guid Id { get; private set; }
+
+    public string Data { get; private set; }
+
+    public string User { get; private set; }
 }
